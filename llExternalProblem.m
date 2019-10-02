10081fc51bfe72d89ee4056dcf8c495c551cd66e
Jesús Mejía <jesusmejded@gmail.com>
2019-10-02 12:49:32 -0500

    minor changes

--- llExternalProblem.m
+++ llExternalProblem.m
@@ -8,10 +8,10 @@ function [functionValue equalityConstrVals inequalityConstrVals] = llExternalPro
     d = ceil(length(x) / 2);
 
     % ff = 10*d + sum((y(1:d) -  x(1:d)).^ 2 - 10*cos( 2*pi*abs(y(1:d) - x(1:d) ) )) ;
-    ff = ( y(1) - x(1) )^2 + 1e6 *sum( (y(2:d) - x(2:d) ).^ 2 ) ;
+    ff = ( y(1) - sin(x(1)) )^2 + 1e6 *sum( (y(2:d) - sin(x(2:d)) ).^ 2 ) ;
 
 
-    p1 = ff - 10*d - sum(  ( x(1:d)  ).^2 - 10*cos( 2*pi * x(1:d) ) );
+    p1 = ff - sum(  x(1:d).^2 );
     p2 = sum((   x(d+1:end) - y(d+1:end)   ).^2) + sum(  x(d+1:end).^2  );
  
     functionValue = p1 + p2;
