1873b4fd0083f70f34bc9d299c43637c594c4bf6
Jesus Mejia <jesusmejded@gmail.com>
2019-10-06 15:44:41 -0500

    pmm

--- ulExternalProblem.m
+++ ulExternalProblem.m
@@ -9,10 +9,10 @@ function [functionValue equalityConstrVals inequalityConstrVals] = ulExternalPro
 
     % ff = 10*d + sum((y(1:d) -  x(1:d)).^ 2 - 10*cos( 2*pi*abs(y(1:d) - x(1:d) ) )) ;
     % ff = 10 + ( y(1)  ) + 1e6 *sum( (y(2:d)).^ 2 ) ;
-    ff = sum( (y(1:d) ).^ 2 ) + (sum( 0.5* (y(1:d) ) ))^2 + (sum( 0.5* (y(1:d) ) ))^4 ;
+    ff = sum( abs(y(1:d) - (x(1)^3)/100).^ (2:d+1) );
 
 
-    p1 = 1e6*abs(ff - x(1)) - sum(  (x(2:d)).^2 );
+    p1 = ff - 10*d - sum(  (x(1:d)).^2 - 10*cos(2*pi*x(1:d)) );
     p2 = sum((  y(d+1:end) - (x(d+1:end) .* sin(x(d+1:end)))   ).^2) + sum(  x(d+1:end).^2  );
     
     functionValue = abs(p1)  + p2;
