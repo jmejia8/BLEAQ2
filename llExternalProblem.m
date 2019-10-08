a07e2eda4aef09169e14821f8dd584249e9b3c34
Jesus Mejia <jesusmejded@gmail.com>
2019-10-08 15:00:40 -0500

    PMM1 is done

--- llExternalProblem.m
+++ llExternalProblem.m
@@ -7,13 +7,23 @@ function [functionValue equalityConstrVals inequalityConstrVals] = llExternalPro
 
     d = ceil(length(x) / 2);
 
+
+    q1 = 10 + y(1) + 1e6*sum( y(2:d).^2 );
+    r1 = -sum(x(1:d).^2);
+
+    q2 = sum( (y(d+1:end) - 0.01*x(d+1:end).^3 ).^2 );
+    r2 = -sum( x(d+1:end).^2 );
+
+    p1 = q1 + r1;
+    p2 = q2 + r2;
+
     % ff = 10*d + sum((y(1:d) -  x(1:d)).^ 2 - 10*cos( 2*pi*abs(y(1:d) - x(1:d) ) )) ;
     % ff = 10 + ( y(1)  ) + 1e6 *sum( (y(2:d)).^ 2 ) ;
-    ff = sum( abs(y(1:d) - (x(1)^3)/100).^ (2:d+1) );
+    % ff = 10*d + sum((y(1:d) -  x(1:d)).^ 2 - 10*cos( 2*pi*abs(y(1:d) - x(1:d) ) )) ;
 
 
-    p1 = ff - 10*d - sum(  (x(1:d)).^2 - 10*cos(2*pi*x(1:d)) );
-    p2 = sum((  y(d+1:end) - (x(d+1:end) .* sin(x(d+1:end)))   ).^2) + sum(  x(d+1:end).^2  );
+    % p1 = ff - (10*d + sum((x(1:d) ).^ 2 - 10*cos( 2*pi*(x(1:d) ) )));
+    % p2 = sum((  y(d+1:end) - (x(d+1:end) ))   ).^2 + sum(  x(d+1:end).^2  );
  
     functionValue = p1 + p2;
 
