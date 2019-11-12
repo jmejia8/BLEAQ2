edee868a85aef2bed8740cff164b1cd4af683b90
Jesus Mejia <jesusmejded@gmail.com>
2019-11-12 10:31:19 -0600

    analysis of PMMs

--- analysis.m
+++ analysis.m
@@ -1,11 +1,21 @@
 function analysis()
 
+ul_evals = zeros(31, 6);
+ll_evals = zeros(31, 6);
 for fnum = 1:6
     i = 0;
     j = 0;
+
+
     for nrun = 1:31
         fname  = sprintf('data/externalProblem_PMM%drun%d.mat',  fnum, nrun);
         therun = load(fname);
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        ul_evals(nrun, fnum) = therun.ulFunctionEvaluations;
+        ll_evals(nrun, fnum) = therun.llFunctionEvaluations;
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         F = therun.ulEliteFunctionValue;
         f = therun.llEliteFunctionValue;
         x = therun.ulEliteIndiv;
@@ -26,4 +36,9 @@ for fnum = 1:6
     success = 100 - ps - nc;
     s = sprintf('  pseudos = %d\t no-converged = %d \t success = %d', ps, nc, success);
     disp(s);
+    disp('==============================================================');
+    disp('==============================================================');
+    disp('==============================================================');
+    disp(mean(ul_evals))
+    disp(mean(ll_evals))
 end
