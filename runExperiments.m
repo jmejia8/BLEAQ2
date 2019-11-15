2f5dbdf733fd3f7562871ccaeada79dd7192b3b5
Jesus Mejia <jesusmejded@gmail.com>
2019-11-15 16:58:51 -0600

    new data path

--- runExperiments.m
+++ runExperiments.m
@@ -3,6 +3,9 @@ function runExperiments()
 D_ul = 5;
 D_ll = 5;
 
+dirname = sprintf('data_%d_%d', D_ul, D_ll);
+mkdir(dirname);
+
 for fn = 1:6
     parfor nrun = 1:31
         externalProblem(fn, nrun, D_ul, D_ll);
